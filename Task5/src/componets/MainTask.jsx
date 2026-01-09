e505d92a0de4dd1dcce1a119dd4d0e45fb0b2fa1
bhagirathsinh9 <bhagirathn.encircle@gmail.com>
2026-01-09 16:28:34 +0530

    feat: add axios instance for fetching products

--- Task5/src/componets/MainTask.jsx
+++ Task5/src/componets/MainTask.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import ProductList from './ProductList';
+import ProductList from './ProductList'
+import api from '../utils/axiosInstance' 
 
 export default function MainTask() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then(res => res.json())
-      .then(data => setProducts(data));
-  }, []);
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get('/products') 
+        setProducts(response.data)
+      } catch (error) {
+        console.error('Error fetching products:', error)
+      }
+    }
+
+    fetchProducts()
+  }, [])
 
   return (
     <div className='max-w-7xl mx-auto'>
       <ProductList products={products} />
     </div>
-);
-
+  )
 }
